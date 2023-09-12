3e483199f14aed6f08216e60e699e387d6c4af52
Matheus H <matheus.henrique4474@gmail.com>
2023-09-12 15:39:26 -0300

    Changed reference of Queries to Repository

--- src/main/java/Handlers/LoginHandler.java
+++ src/main/java/Handlers/LoginHandler.java
@@ -3,21 +3,27 @@ package Handlers;
 import Commands.LoginCommand;
 import Enums.EResponseTypes;
 import Handlers.Contract.HandlerContract;
+import Models.Admin;
+import Models.Resident;
 import Models.ResponseModel;
-import Queries.AdminQueries;
-import Queries.ResidentQueries;
+import Repository.AdminRepo;
+import Repository.ResidentRepo;
 
 public class LoginHandler implements HandlerContract<ResponseModel<Boolean>, LoginCommand> {
 
     @Override
     public ResponseModel<Boolean> handle(LoginCommand input) {
-        Object response1 = new AdminQueries().loginQuery(input.getUsername(), input.getPassword());
-        Object response2 = new ResidentQueries().loginQuery(input.getUsername(), input.getPassword());
+        //FIXME: probably i should inject the repo instead of instantiate it
+
+        Admin response1 = new AdminRepo().loginQuery(input.getUsername(), input.getPassword());
+        Resident response2 = new ResidentRepo().loginQuery(input.getUsername(), input.getPassword());
 
         if(response1 == null && response2 == null){
-            return new ResponseModel<>(EResponseTypes.NullQuery.name(),
-                    false, null, false);
+            return new ResponseModel<>(input.toString(),
+                    false, new String[] {EResponseTypes.NullQuery.name()}, false);
         }
 
+        return new ResponseModel<>(input.toString(),
+                true, new String[] {EResponseTypes.ValidQuery.name()}, true);
     }
 }
