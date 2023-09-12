59bc1e303f0c047452504ce1e47f23d19c9dcc4a
Matheus H <matheus.henrique4474@gmail.com>
2023-09-12 15:33:02 -0300

    Removed some imports and @NotNull

--- src/main/java/Controllers/Login.java
+++ src/main/java/Controllers/Login.java
@@ -2,19 +2,11 @@ package Controllers;
 
 
 import Commands.LoginCommand;
-import Controllers.Contract.ControllerContract;
 import Enums.EResponseTypes;
 import Handlers.LoginHandler;
-import Models.Resident;
 import Models.ResponseModel;
-import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,10 +16,10 @@ public class Login  {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public ResponseModel<Boolean> login(@NotNull LoginCommand command) {
+    public ResponseModel<Boolean> login(LoginCommand command) {
         command.validate();
         if(command.isInvalid()){
-            return new ResponseModel<Boolean>(EResponseTypes.InvalidData.toString(), false, null, false);
+            return new ResponseModel<>(EResponseTypes.InvalidData.toString(), false, null, false);
         }
 
         LoginHandler handler = new LoginHandler();
